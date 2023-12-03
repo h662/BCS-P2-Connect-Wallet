acaf58e2d3e7daef1950669730e66e1d0e381322
h662 <h662hong@gmail.com>
2023-12-03 14:27:04 +0900

    connect metamask

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,28 @@
+import { useSDK } from "@metamask/sdk-react";
+
 const App = () => {
-  return <div className="bg-red-100">Hello, React!</div>;
+  const { sdk } = useSDK();
+
+  const onClickMetamask = async () => {
+    try {
+      const accounts = await sdk?.connect();
+
+      console.log(accounts);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <div className="min-h-screen flex justify-center items-center">
+      <button
+        className="bg-blue-600 text-white px-4 py-2 rounded-lg text-xl"
+        onClick={onClickMetamask}
+      >
+        Connect Metamask
+      </button>
+    </div>
+  );
 };
 
 export default App;
