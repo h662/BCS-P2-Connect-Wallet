d62271da2372b146aaab3a69c50fc30062ab2fe0
h662 <h662hong@gmail.com>
2023-12-03 14:32:31 +0900

    View Account

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import { useSDK } from "@metamask/sdk-react";
+import { useState } from "react";
 
 const App = () => {
+  const [account, setAccount] = useState("");
+
   const { sdk } = useSDK();
 
   const onClickMetamask = async () => {
     try {
       const accounts = await sdk?.connect();
 
-      console.log(accounts);
+      setAccount(accounts[0]);
     } catch (error) {
       console.error(error);
     }
@@ -15,12 +18,19 @@ const App = () => {
 
   return (
     <div className="min-h-screen flex justify-center items-center">
-      <button
-        className="bg-blue-600 text-white px-4 py-2 rounded-lg text-xl"
-        onClick={onClickMetamask}
-      >
-        Connect Metamask
-      </button>
+      {account ? (
+        <span>
+          Hello, {account.substring(0, 7)}...
+          {account.substring(account.length - 5)}
+        </span>
+      ) : (
+        <button
+          className="bg-blue-600 text-white px-4 py-2 rounded-lg text-xl"
+          onClick={onClickMetamask}
+        >
+          Connect Metamask
+        </button>
+      )}
     </div>
   );
 };
